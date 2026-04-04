e027122ba3cd2ff052ea080d749daee12b6051a1
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-04-05 00:11:04 +0530

    Series question

---
+++ Java_DSA/forloop/forloop1.java
@@ -0,0 +1,12 @@
+package forloop;
+
+public class forloop1 {
+    public static void main(String[] args) {
+        //print this series 99 95 91 87 ... upto all terms which are positive
+        int a = 99;
+        for (int i =a; i>=0; i=i-4) {
+            System.out.println(i);
+            }
+
+    }
+}
