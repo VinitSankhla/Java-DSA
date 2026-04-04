0bce486c09116e1322d0d6ae419411c90db71a90
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-04-05 01:29:27 +0530

    Ascii values of alphabets

--- Java_DSA/forloop/forloop3.java
+++ Java_DSA/forloop/forloop3.java
@@ -2,6 +2,9 @@ package forloop;
 
 public class forloop3 {
     public static void main(String[] args) {
-        
+        for (int i = 'A'; i <= 'Z'; i++) {
+            System.out.print((char)i+":");
+            System.out.println(i);
+        }
     }
 }
