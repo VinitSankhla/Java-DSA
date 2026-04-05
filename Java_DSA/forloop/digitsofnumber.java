c9c8426e1252f4c2e38b4effb3156ccc31738f66
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-04-06 01:04:13 +0530

    Added sum of digits

--- Java_DSA/forloop/digitsofnumber.java
+++ Java_DSA/forloop/digitsofnumber.java
@@ -9,15 +9,16 @@ public class digitsofnumber {
         System.out.println("Enter the number to find the number of digits in it");
         int a = sc.nextInt();
         int count = 0;
-        
+        int sum = 0;
         if(a==0) a=1;//base case of a = 0 as 0 is a digit
         while(a!=0){
             count++;
+            sum +=(a%10);
             a/=10;
-            
         }
         System.out.println("The count of the digits is : "+count);
-        
+        System.out.print("The sum of digits is : ");
+        System.out.println((sum>0) ? sum : -sum);
         sc.close();
     }
 }
