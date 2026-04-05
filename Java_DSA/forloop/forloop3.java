505e4af059985cd77383ae374786dccc8f23a206
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-04-05 23:30:49 +0530

    Added question

--- Java_DSA/forloop/forloop3.java
+++ Java_DSA/forloop/forloop3.java
@@ -2,6 +2,7 @@ package forloop;
 
 public class forloop3 {
     public static void main(String[] args) {
+        // print ascii values for all alphabets 
         for (int i = 'A'; i <= 'Z'; i++) {
             System.out.print((char)i+":");
             System.out.println(i);
