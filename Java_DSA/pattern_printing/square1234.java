13aa261908bdef5400438369ac68814650e284a2
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-04-07 23:26:38 +0530

    square 1234 pattern

---
+++ Java_DSA/pattern_printing/square1234.java
@@ -0,0 +1,13 @@
+package pattern_printing;
+
+public class square1234 {
+    public static void main(String[] args) {
+        int x = 4;
+        for (int i=0;i<x;i++){
+            for (int j=1;j<=4;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
