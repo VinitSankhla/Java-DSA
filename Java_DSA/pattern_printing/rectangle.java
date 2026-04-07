a2b1fe1632251bc92b2952d02dc76d8b4b689148
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-04-07 23:19:06 +0530

    User input added

--- Java_DSA/pattern_printing/rectangle.java
+++ Java_DSA/pattern_printing/rectangle.java
@@ -1,14 +1,20 @@
 package pattern_printing;
 
+import java.util.Scanner;
+
 public class rectangle {
     public static void main(String[] args) {
-        int x = 3;
-        int y = 6;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows :");
+        int x = sc.nextInt();
+        System.out.print("Enter number of columns :");
+        int y = sc.nextInt();
         for(int i=0;i<x;i++){
             for(int j=0;j<y;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
+        sc.close();
     }
 }
