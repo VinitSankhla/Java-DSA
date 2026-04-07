516b68e7126753e2aeeb809dcde9a1921827693c
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-04-07 23:35:34 +0530

    Small case alphabet square

---
+++ Java_DSA/pattern_printing/smallcasealphasquare.java
@@ -0,0 +1,18 @@
+package pattern_printing;
+
+import java.util.Scanner;
+
+public class smallcasealphasquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Give the Length of the alphabets :");
+        int x = sc.nextInt();
+        for(int i=1;i<=x;i++){
+            for(int j=1;j<=x;j++){
+                System.out.print((char)(j+96)+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
