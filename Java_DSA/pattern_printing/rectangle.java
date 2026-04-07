e634444125c25fab496cccc613c04a990f397592
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-04-07 23:16:08 +0530

    pattern printing

---
+++ Java_DSA/pattern_printing/rectangle.java
@@ -0,0 +1,14 @@
+package pattern_printing;
+
+public class rectangle {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 6;
+        for(int i=0;i<=x;i++){
+            for(int j=0;j<=y;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
