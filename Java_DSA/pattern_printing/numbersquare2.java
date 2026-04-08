bb18c92e8825b9860f8d2a202879e29b49134006
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-04-08 23:32:15 +0530

    added user input

--- Java_DSA/pattern_printing/numbersquare2.java
+++ Java_DSA/pattern_printing/numbersquare2.java
@@ -1,13 +1,18 @@
 package pattern_printing;
 
+import java.util.Scanner;
+
 public class numbersquare2 {
     public static void main(String[] args) {
-        int x = 4;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int x = sc.nextInt();
         for (int i=1;i<=x;i++){
             for(int j=1;j<=x;j++){
                 System.out.print(i+" ");
             }
             System.out.println();
         }
+    sc.close();
     }
 }
