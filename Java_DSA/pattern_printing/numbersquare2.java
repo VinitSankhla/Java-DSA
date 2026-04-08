9ee70a46cc1f59c93873cdf16796d8f96203447f
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-04-08 23:30:11 +0530

    number square pattern 2

---
+++ Java_DSA/pattern_printing/numbersquare2.java
@@ -0,0 +1,13 @@
+package pattern_printing;
+
+public class numbersquare2 {
+    public static void main(String[] args) {
+        int x = 4;
+        for (int i=1;i<=x;i++){
+            for(int j=1;j<=x;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
