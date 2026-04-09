c4316f80a87cbdb1029e9397fc69450847e026e6
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-04-09 23:29:58 +0530

    user input taken

--- Java_DSA/pattern_printing/flippedtriangle.java
+++ Java_DSA/pattern_printing/flippedtriangle.java
@@ -1,13 +1,18 @@
 package pattern_printing;
 
+import java.util.Scanner;
+
 public class flippedtriangle {
     public static void main(String[] args) {
-        int x = 4;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a value");
+        int x = sc.nextInt();
         for (int i=1;i<=x;i++){
             for (int j=1;j<=i;j++){
                 System.out.print("* ");
             }
             System.out.println();
         }
+        sc.close();
     }
 }
