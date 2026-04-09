ecbc669760349c9fdc45cc3a28cd5ad0d88e1567
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-04-09 23:33:28 +0530

    reversed triangle properly implemented

--- Java_DSA/pattern_printing/flippedtriangle.java
+++ Java_DSA/pattern_printing/flippedtriangle.java
@@ -7,7 +7,7 @@ public class flippedtriangle {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a value");
         int x = sc.nextInt();
-        for (int i=1;i<=x;i++){
+        for (int i=x;i>=0;i--){
             for (int j=1;j<=i;j++){
                 System.out.print("* ");
             }
