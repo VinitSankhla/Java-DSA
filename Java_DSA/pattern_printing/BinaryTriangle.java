ba29ed568ecf17c2599a7f33f859511e23cd3389
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-04-11 15:57:14 +0530

    removed unnecessary conditions

--- Java_DSA/pattern_printing/BinaryTriangle.java
+++ Java_DSA/pattern_printing/BinaryTriangle.java
@@ -8,17 +8,19 @@ public class BinaryTriangle {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows you want to print in Binary Triangle : ");
         int x = sc.nextInt();
-        outerloop:
+        if (x<=0) {
+            System.out.println("Enter a positive value only !");
+            sc.close();
+            return;
+        }
         for (int i=1;i<=x;i++){
                 for (int j=1;j<=i;j++){
-                    if (a<=x) {
                         System.out.print(a + " ");
                         a^=1;
-                    }
-                    else break outerloop;
             }
             System.out.println();
         }
         sc.close();
     }
 }
+
