7e2bd4537e82fe8b610214d077bf3f1e312c92b7
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-04-11 15:16:01 +0530

    Star cross implementation

--- Java_DSA/pattern_printing/StarCross.java
+++ Java_DSA/pattern_printing/StarCross.java
@@ -2,6 +2,16 @@ package pattern_printing;
 
 public class StarCross {
     public static void main(String[] args) {
-        
+        int x = 5;
+        for (int i=0;i<x;i++){
+            for (int j=0;j<x;j++){
+                if (i==j || (i+j)==4) {
+                    System.out.print("* ");
+                }
+                else System.out.print("  ");
+            }
+            System.out.println();
+        }
+
     }
 }
