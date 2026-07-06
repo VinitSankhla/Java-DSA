1a5d3b220654d09b655f3065d18a031745e54e75
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-07-07 02:00:10 +0530

    methods - builtin

---
+++ Java_DSA/methods/Builtin.java
@@ -0,0 +1,13 @@
+package methods;
+
+public class Builtin {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(100));
+        System.out.println(Math.cbrt(100));
+        System.out.println(Math.floor(-7.8));
+        System.out.println(Math.ceil(3.001));
+        System.out.println(Math.max(3.56, 9));
+        System.out.println(Math.min(2.4, 2.41));
+        System.out.println(Math.pow(3.12, 2));
+    }
+}
