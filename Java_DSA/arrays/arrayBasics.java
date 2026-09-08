e7fcdd664310e0cc808a56bafa7b0d031eeec2c2
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-09-09 00:00:04 +0530

    array basics

---
+++ Java_DSA/arrays/arrayBasics.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class arrayBasics {
+    public static void main(String[] args) {
+        int[] x = {6,19,7,65,98,6,5};
+        // indexing 
+        System.out.println(x[0]); //access
+        //updating elements - mutable
+        x[0]=10;
+        System.out.println(x[0]);
+        int[] arr = new int[4]; //array of length 4 | 0 to 3
+        arr[0]=10;
+        
+    }
+}
