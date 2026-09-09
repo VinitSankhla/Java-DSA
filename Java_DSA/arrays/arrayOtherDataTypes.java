ff4d057173efac92bb7cfd934e55cb84391b3fb1
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-09-10 00:30:18 +0530

    Different data types arrays

--- Java_DSA/arrays/arrayOtherDataTypes.java
+++ Java_DSA/arrays/arrayOtherDataTypes.java
@@ -2,6 +2,14 @@ package arrays;
 
 public class arrayOtherDataTypes {
     public static void main(String[] args) {
-        
+        double[] arr = new double[8];// default value 0.0
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println(" ");
+        String[] arr1 = new String[8];
+        for(int i=0;i<arr1.length;i++){
+            System.out.println(arr[i] + " ");
+        }
     }
 }
