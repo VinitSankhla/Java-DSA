6eebbf8751d4fc12c9df1445708d8fc89c79d5d1
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-09-10 01:17:20 +0530

    Multiply odd indexed elements by 2 and add 10 to even indexed elements

--- Java_DSA/arrays/oddIndexEvenIndexoperation.java
+++ Java_DSA/arrays/oddIndexEvenIndexoperation.java
@@ -1,5 +1,17 @@
 package arrays;
 
 public class oddIndexEvenIndexoperation {
-    
+    public static void main(String[] args) {
+        //Multiply odd indexed elements by 2 and add 10 to even indexed elements 
+        int[] arr = {10,20,30,40,50,60};
+        for(int i=0;i<arr.length;i++){
+            if (i%2==0){
+                arr[i]=10+arr[i];
+            }
+            else{arr[i]=2*arr[i];}
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
 }
