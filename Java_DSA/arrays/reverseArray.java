ff9fcae3ad640a4610ce6126777c967ae8594026
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-09-11 02:10:09 +0530

    Reverse the array using 2 variable technique

--- Java_DSA/arrays/reverseArray.java
+++ Java_DSA/arrays/reverseArray.java
@@ -3,9 +3,13 @@ package arrays;
 public class reverseArray {
     public static void main(String[] args) {
         int [] arr = {6,8,1,2,4,9};
-        int i = 0;
-        int j = arr.length-1;
-        while(j>i){
+        for(int k : arr){
+            System.out.print(k+" ");
+        }
+        System.out.println();
+        int i = 0;//will access from the front
+        int j = arr.length-1;//will access from the end
+        while(j>i){//till the front and the end meets 
             swap(arr,i, j);
             i++;
             j--;
