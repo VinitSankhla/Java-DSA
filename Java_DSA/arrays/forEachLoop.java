f08ae5c71563f7e1cb23c5c4847a84509a73a08b
Vinit Sankhla <vinitsankhla2006@gmail.com>
2026-09-11 01:46:09 +0530

    for each loop basics

---
+++ Java_DSA/arrays/forEachLoop.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class forEachLoop {
+    public static void main(String[] args) {//just for traversing the array and can not change the elements
+        int[] arr= {10,20,30,40,50,60,70,80};//mainly used for printing, read, access
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        for(int i : arr){
+            i *= 2;//this will not happen as it is not possible with for each loop
+        }
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
+    }
+}
